06893f542d9a5fc15ade44206ad0a2a15640ce85
Nijesh Shrestha <anishnijesh117@gmail.com>
2026-08-09 16:28:11 +0545

    runner for adk set up and working on authorization

---
+++ backend/app/routes/chat.py
@@ -0,0 +1,48 @@
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from google.genai import types
+
+from app.agent.runner import runner
+from backend.app.services.gmail import get_current_user
+
+router = APIRouter()
+
+
+class ChatRequest(BaseModel):
+    message: str
+    session_id: str
+
+
+@router.post("/chat")
+async def chat(request: ChatRequest, current_user = Depends(get_current_user)):
+    user_id = current_user.id
+    
+    content = types.Content(
+        role="user",
+        parts=[
+            types.Part(
+                text=request.message
+            )
+        ],
+    )
+
+    events = runner.run_async(
+        user_id=user_id,
+        session_id=request.session_id,
+        new_message=content,
+    )
+
+    final_response = None
+
+    async for event in events:
+
+        if event.is_final_response():
+
+            if event.content and event.content.parts:
+
+                final_response = event.content.parts[0].text
+
+    return {
+        "session_id": request.session_id,
+        "response": final_response,
+    }
